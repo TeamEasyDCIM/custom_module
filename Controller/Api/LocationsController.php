942555602d2f9455da79c330a91bc1abd4dffb34
Konrad Bieda <konrad.bi@modulesgarden.com>
2019-02-12 08:56:48 +0100

    INT: API tests

---
+++ Controller/Api/LocationsController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Modules\Addons\CustomModule\Controller\Api;
+use Modules\Addons\CustomModule\Controller\OutputController;
+
+/**
+ * Class LocationsController
+ * @package Modules\Addons\CustomModule\Controller\Api
+ */
+class LocationsController extends OutputController
+{
+    /**
+     * @return string|void
+     */
+    public function createLocation()
+    {
+        /**
+         * Endpoint URL
+         */
+        $url = url('api/v2/location');
+
+        /**
+         * First Admin API Key - you should enter your own user api key
+         */
+        $apikey = \User::admins()->first()->apikey->key;
+
+        $client = new \GuzzleHttp\Client();
+
+        try {
+            $response = $client->request('POST', $url, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'apikey' => $apikey
+                ],
+                'json' => [
+                    'data' => [
+                        'name' => 'New York',
+                        'address' => 'York Street Subway Station',
+                        'lat' => '40.7013482',
+                        'lng' => '-73.9866001'
+                    ],
+                ]
+            ]);
+
+            $stream = \GuzzleHttp\Psr7\stream_for($response->getBody());
+
+            d($stream->getContents());
+        } catch (\Exception $e) {
+            d($e->getMessage());
+        }
+    }
+
+    /**
+     * @return string|void
+     */
+    public function createFloor()
+    {
+        /**
+         * Endpoint URL
+         */
+        $url = url('api/v2/floor');
+
+        /**
+         * First Admin API Key - you should enter your own user api key
+         */
+        $apikey = \User::admins()->first()->apikey->key;
+
+        $client = new \GuzzleHttp\Client();
+
+        try {
+            $response = $client->request('POST', $url, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'apikey' => $apikey
+                ],
+                'json' => [
+                    'data' => [
+                        'name' => 'Test Floor 1',
+                        'number' => 2,
+                        'location_id' => 1,
+                    ],
+                ]
+            ]);
+
+            $stream = \GuzzleHttp\Psr7\stream_for($response->getBody());
+
+            d($stream->getContents());
+        } catch (\Exception $e) {
+            d($e->getMessage());
+        }
+    }
+
+    /**
+     * @return string|void
+     */
+    public function createRack()
+    {
+        /**
+         * Endpoint URL
+         */
+        $url = url('api/v2/rack');
+
+        /**
+         * First Admin API Key - you should enter your own user api key
+         */
+        $apikey = \User::admins()->first()->apikey->key;
+
+        $client = new \GuzzleHttp\Client();
+
+        try {
+            $response = $client->request('POST', $url, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'apikey' => $apikey
+                ],
+                'json' => [
+                    'data' => [
+                        'name' => 'Test Rack 1',
+                        'floor_id' => 4,
+                        'location_id' => 1,
+                        'units_number' => 10
+                    ],
+                ]
+            ]);
+
+            $stream = \GuzzleHttp\Psr7\stream_for($response->getBody());
+
+            d($stream->getContents());
+        } catch (\Exception $e) {
+            d($e->getMessage());
+        }
+    }
+}
